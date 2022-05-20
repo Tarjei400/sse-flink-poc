0c3148bb5124cd2b8a08d15bc7b94a8b34939c7f
Maciej Baruch <maciej.baruch@lunchgate.com>
2022-05-20 14:13:14 +0200

    feat: add sse event listeners

--- frontend/file.js
+++ frontend/file.js
@@ -1,1 +1,16 @@
-console.log("ddd")
+console.log("INIT JS")
+
+const MERCURE_HUB_URL = 'http://localhost:8081/.well-known/mercure'
+const FRONT_TOPIC = 'front'
+const TOPIC_PARAM_NAME = 'topic'
+
+const sseInit = () => {
+  const url = new URL(MERCURE_HUB_URL);
+  url.searchParams.append(TOPIC_PARAM_NAME, FRONT_TOPIC);
+
+  const eventSource = new EventSource(url)
+  eventSource.addEventListener('event1', event => console.log("event1", event))
+  eventSource.addEventListener('event2', event => console.log("event2", event))
+}
+
+sseInit()
